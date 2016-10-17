c5a5b2f8d4a90285e217c16bb66a4b86e89dd756
Hughes89 <mik3h89@gmail.com>
2016-10-17 19:54:25 -0400

    currently working with bower

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
-var db;
+
 var port = 8080;
+
 app.use(express.static('../public'));
 
 app.get('/', function (req, res) {
   res.sendFile('../public/index.html');
 });
 
-mongoose.connect('mongodb://uname:pword@ds045521.mlab.com:45521/comment-box')
-db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
+// var db;
+// mongoose.connect('mongodb://Mike:test123@ds045521.mlab.com:45521/comment-box')
+// db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error'));
 
 
 app.listen(8080);
