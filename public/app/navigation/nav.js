6aaaae05798d71999dce1332fcb4238814cdee7d
Hughes89 <mik3h89@gmail.com>
2016-10-18 11:38:46 -0400

    Fills in dropdown with lifts

--- public/app/navigation/nav.js
+++ public/app/navigation/nav.js
@@ -3,7 +3,14 @@ angular.module('track.nav', [])
 .controller('navController', function ($scope, $stateParams, $http, userData) {
   $scope.data = {};
   $scope.user = window.user;
-  $scope.workout = $stateParams;
-  //console.log($stateParams);
- 
+  $scope.data.liftList = [];
+  $scope.makeUnique = function (array) {
+    array.forEach(function (ele) {
+      if ($scope.data.liftList.indexOf(ele.lift) === -1) {
+        $scope.data.liftList.push(ele.lift);
+      }
+    });
+  };
+  $scope.getLifts = userData.getData;
+  $scope.getLifts($scope.makeUnique);
 });
