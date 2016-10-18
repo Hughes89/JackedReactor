25fb5066c08b71f89f0da0279a3b8a970bdaee01
Hughes89 <mik3h89@gmail.com>
2016-10-17 22:12:00 -0400

    Working, starting db

--- server/server.js
+++ server/server.js
@@ -6,15 +6,45 @@ var port = 8080;
 
 app.use(express.static('../public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile('../public/index.html');
 });
 
-// var db;
-// mongoose.connect('mongodb://Mike:test123@ds045521.mlab.com:45521/comment-box')
-// db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error'));
+app.get('/user/:id', function (req, res, next) {
+  var user = req.params.id;
+  Lift.find({ user : user }, function (err, liftObj) {
+    if (err) {
+      console.log(err);
+    } else if (liftObj) {
+      console.log('Found: ' + liftObj);
+      res.json(liftObj);
+    }
+  });
+});
 
+var db;
+mongoose.connect('mongodb://Mike:test123@ds045521.mlab.com:45521/comment-box');
+
+//Saving Lift :
+var Lift = mongoose.model('Lift', {lift: String, date: String, weight: Number, user: String});
+// var test = new Lift({ lift: 'Bench', date: 'tom', weight: 180, user: 'mike' });
+// test.save(function (err, liftObj) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Save successful: ', liftObj);
+//   }
+// });
+
+//Finding User Lifts : 
+// Lift.find({ user : 'mike' }, function (err, liftObj) {
+//   if (err) {
+//     console.log(err);
+//   } else if (liftObj) {
+//     console.log('Found: ' + liftObj);
+//   }
+// });
 
 app.listen(8080);
 
