ccc6043a91d54623d18bf1441e28bbb8030785dc
Hughes89 <mik3h89@gmail.com>
2016-10-18 11:37:45 -0400

    edited the directive

--- public/app/app.js
+++ public/app/app.js
@@ -20,14 +20,9 @@ angular.module('track', ['track.nav', 'track.add', 'track.lift', 'track.services
   })
 })
 
-.config(function($mdIconProvider) {
-    $mdIconProvider
-      .iconSet("call", 'img/icons/sets/communication-icons.svg', 24)
-      .iconSet("social", 'img/icons/sets/social-icons.svg', 24);
-  })
+
   .controller('PositionDemoCtrl', function DemoCtrl($mdDialog) {
     var originatorEv;
-    this.menuHref = "http://www.google.com/design/spec/components/menus.html#menus-specs";
     this.openMenu = function($mdOpenMenu, ev) {
       originatorEv = ev;
       $mdOpenMenu(ev);
