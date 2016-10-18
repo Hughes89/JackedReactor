e9765e12ee98e30dfcdede0f98741fdc98bee58b
Hughes89 <mik3h89@gmail.com>
2016-10-18 11:37:20 -0400

    Added 1RM calc function"

--- public/app/add/add.js
+++ public/app/add/add.js
@@ -3,12 +3,19 @@ angular.module('track.add', [])
 .controller('addController', function ($scope, $stateParams, $http, userData) {
   $scope.lift = '';
   $scope.weight = '';
+  $scope.reps = '';
+
   $scope.sendLift = function () {
-    var storage = [window.user, $scope.lift, $scope.weight];
-    return $http({
+    $scope.total = Math.round($scope.weight * (1 + ($scope.reps/30)));
+    var storage = [window.user, $scope.lift, $scope.total];
+    $scope.lift = '';
+    $scope.weight = '';
+    $scope.reps = '';
+    $http({
       method: 'POST',
       url: '/submitLift',
       data: storage
     });
   };
+
 });
