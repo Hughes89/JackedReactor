4f747ef9bac893e155c042450b3b877c4cdead7f
Hughes89 <mik3h89@gmail.com>
2016-10-18 01:55:09 -0400

    fixed bugs

--- public/app/navigation/nav.js
+++ public/app/navigation/nav.js
@@ -1,19 +1,9 @@
 angular.module('track.nav', [])
 
 .controller('navController', function ($scope, $stateParams, $http, userData) {
-  userData.getData();
   $scope.data = {};
-  $scope.hello = 'Hello';
-  
-  
-  $scope.lift = '';
-  $scope.weight = '';
-  $scope.sendLift = function () {
-    var storage = [window.user, $scope.lift, $scope.weight];
-    return $http({
-      method: 'POST',
-      url: '/submitLift',
-      data: storage
-    });
-  };
+  $scope.user = window.user;
+  $scope.workout = $stateParams;
+  //console.log($stateParams);
+ 
 });
