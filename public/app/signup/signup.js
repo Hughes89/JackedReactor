774730b17b893f387d03b115bfe9471c1a74eb14
Hughes89 <mik3h89@gmail.com>
2016-10-18 18:47:43 -0400

    signup working

--- public/app/signup/signup.js
+++ public/app/signup/signup.js
@@ -1,5 +1,14 @@
 angular.module('track.signup', [])
 
-.controller('signupController', function ($scope, $stateParams, $http, $location, userData) {
+.controller('signupController', function ($scope, $stateParams, $http, $location, Auth) {
+  $scope.user = {};
+  $scope.err = false;
 
+  $scope.signup = function () {
+    if ($scope.user.password.length >= 8) {
+      Auth.signup($scope.user);
+    } else {
+      $scope.err = true;
+    }
+  };
 });
