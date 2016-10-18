e992ff084f43919247a27063abc35f4d0919d7e0
Hughes89 <mik3h89@gmail.com>
2016-10-18 15:39:57 -0400

    added delete request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var moment = require('moment');
 var app = express();
 
 var port = 8080;
@@ -57,13 +58,25 @@ app.get('/:user/:id', function (req, res, next) {
 
 app.post('/submitLift', function (req, res, next) {
   var data = req.body;
-  var lift = new Lift({ lift: data[1], date: Date(), weight: data[2], user: data[0] });
+  var date = moment().format('MM/DD/YYYY');
+  var lift = new Lift({ lift: data[1], date: date, weight: data[2], user: data[0] });
   lift.save(function (err, liftObj) {
     if (err) {
       console.log(err);
     } else {
       console.log('Save successful: ', liftObj);
     }
+  });
+});
+
+app.delete('/delete/:user/:lift', function (req, res, next) {
+  var lift = req.params.lift;
+  var user = req.params.user;
+  Lift.find({ user : user, lift: lift }).remove().exec(function (err, data) {
+    if (err) {
+      console.log(egg)
+    }
+    res.sendStatus(201);
   });
 });
 
