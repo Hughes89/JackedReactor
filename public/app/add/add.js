377dcc36645d786c18543b8ef917afce855eed58
Hughes89 <mik3h89@gmail.com>
2016-12-07 11:27:12 -0500

    Feat: clean up add controller

--- public/app/add/add.js
+++ public/app/add/add.js
@@ -1,24 +1,27 @@
-angular.module('track.add', [])
-
-.controller('addController', function ($scope, $stateParams, $http, $location, userData, Auth) {
-  $scope.lift = '';
-  $scope.weight = '';
-  $scope.reps = '';
-  $scope.sendLift = function () {
-    var storage = {
-      lift: $scope.lift.toLowerCase(),
-      weight: $scope.weight,
-      reps: $scope.reps
-    };
-    $http({
-      method: 'POST',
-      url: '/api/submitLift',
-      data: storage
-    });
-    var path = $scope.lift.toLowerCase();
-    var first = path[0].toUpperCase();
-    $location.path('/' +  first + path.substr(1));
-  };
+(function() {
+  'use strict';
   
+  angular
+    .module('track.add', [])
+    .controller('addController', addController);
+    
+  addController.$inject = ['$stateParams', '$http', '$location', 'userData', 'Auth'];
 
-});
+  function addController ($stateParams, $http, $location, userData, Auth) {
+    /*jshint validthis: true */
+    var vm = this;
+    vm.lift = '';
+    vm.weight = '';
+    vm.reps = '';
+    vm.sendLift = sendLift;
+    
+    function sendLift () {
+      var storage = {
+        lift: vm.lift.toLowerCase(),
+        weight: vm.weight,
+        reps: vm.reps
+      };
+      userData.submitLift($http, storage);
+    }
+  }
+})();
