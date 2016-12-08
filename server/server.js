c22f6fcfc0415682f2b17d4013c3374d51cb1a2c
Hughes89 <mik3h89@gmail.com>
2016-12-07 23:00:09 -0500

    bug: dirname to serv static

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 //Middleware
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 app.use("/node_modules", express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
